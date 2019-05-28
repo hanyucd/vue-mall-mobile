e05fd4afa512b8c3367390e570a366263ef9a935
han yu <1807003141@qq.com>
2019-05-28 21:18:24 +0800

    feat: 新增侧边栏导航下划线跟随效果.

--- mall/src/pages/Category/index.vue
+++ mall/src/pages/Category/index.vue
@@ -3,11 +3,14 @@
     <top-bar title="商品分类"></top-bar>
     <!-- 内容区 -->
     <div class="content">
+      <!-- 侧边栏导航 -->
       <section class="left">
         <ul>
           <li v-for="(item, index) in categoryList" :key="item.mallCategoryId" :class="{ active: sidebarIndex === index }" @click="switchSideNav(index)">
             {{ item.mallCategoryName }}
           </li>
+          <!-- 侧边栏导航标记 -->
+          <article class="sidebar-tag" ref="sideTagRef"></article>
         </ul>
       </section>
     </div>
@@ -40,13 +43,12 @@
        */
       async _getCategoryList() {
         // 调用 Vuex 中的 Getters | 值在 GoodsMixin 中
-        // console.log(this.categoryList)
         if (!this.categoryList.length) {
           try {
             let res = await ajax.getHomeData();
             if (res.code === 200) {
               // 调用 Vuex 中的 Actions | 方法在 GoodsMixin 中
-              this.setCategoryList(res.result.category)
+              this.setCategoryList(res.result.category);
             }
           } catch (error) {
             console.log(error);
@@ -59,6 +61,9 @@
       switchSideNav(index) {
         if (this.sidebarIndex === index) return;
         this.sidebarIndex = index;
+        // 计算侧边导航标签距离顶部距离
+        let top = 100 / (this.categoryList.length) * index;
+        this.$refs.sideTagRef.style.top = `${ top }%`;
       }
     }
   }
