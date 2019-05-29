6240e4624ccc6f448123c290a83cea35d3a4e25f
han yu <1807003141@qq.com>
2019-05-29 18:08:01 +0800

    feat: 新增子分类列表数据.

--- mall/src/pages/Category/index.vue
+++ mall/src/pages/Category/index.vue
@@ -3,15 +3,27 @@
     <top-bar title="商品分类"></top-bar>
     <!-- 内容区 -->
     <div class="content">
-      <!-- 侧边栏导航 -->
+      <!-- 左侧导航栏 -->
       <section class="left">
         <ul>
-          <li v-for="(item, index) in categoryList" :key="item.mallCategoryId" :class="{ active: sidebarIndex === index }" @click="switchSideNav(index)">
+          <li v-for="(item, index) in categoryList" :key="item.mallCategoryId" :class="{ active: sidebarIndex === index }" @click="switchSideNav(item, index)">
             {{ item.mallCategoryName }}
           </li>
           <!-- 侧边栏导航标记 -->
           <article class="sidebar-tag" ref="sideTagRef"></article>
         </ul>
+      </section>
+      <!-- 右侧内容 -->
+      <section class="right">
+        <van-tabs v-model="currentTabIndex">
+          <!-- <b-scroll> -->
+            <div class="container">
+              <van-tab v-for="(item, index) in categorySubList" :key="index" :title="item.mallSubName">
+                h
+              </van-tab>
+            </div>
+          <!-- </b-scroll> -->
+        </van-tabs>
       </section>
     </div>
     <!-- 底部导航 -->
@@ -21,6 +33,7 @@
 
 <script>
   import TopBar from '@/components/TopBar';
+  import BScroll from '@/components/BScroll';
   import FooterNav from '@/components/FooterNav';
   import { GoodsMixin } from '@/mixins/goodsMixin';
   import ajax from '@/api';
@@ -28,10 +41,12 @@
   export default {
     name: 'Category',
     mixins: [ GoodsMixin ],
-    components: { TopBar, FooterNav },
+    components: { TopBar, BScroll, FooterNav },
     data() {
       return {
         sidebarIndex: 0, // 侧边栏导航下标
+        categorySubList: [], // 子分类列表
+        currentTabIndex: 0, // 当前子分类激活下标
       };
     },
     created() {
@@ -42,13 +57,15 @@
        * 获取分类列表 & 存到 Vuex 中
        */
       async _getCategoryList() {
-        // 调用 Vuex 中的 Getters | 值在 GoodsMixin 中
-        if (!this.categoryList.length) {
+        // 判断是否已存在大分类列表 | 调用 Vuex 中的 Getters，值在 GoodsMixin 中
+        if (this.categoryList.length) {
           try {
             let res = await ajax.getHomeData();
             if (res.code === 200) {
-              // 调用 Vuex 中的 Actions | 方法在 GoodsMixin 中
+              // 设置大分类列表 | 调用 Vuex 中的 Actions，方法在 GoodsMixin 中
               this.setCategoryList(res.result.category);
+              // 默认设置第一个大分类的子分类列表
+              this.categorySubList = res.result.category[0].bxMallSubDto;
             }
           } catch (error) {
             console.log(error);
@@ -58,12 +75,17 @@
       /**
        * 切换侧边导航
        */
-      switchSideNav(index) {
+      switchSideNav(item, index) {
         if (this.sidebarIndex === index) return;
+        // 切换左侧导航下标
         this.sidebarIndex = index;
         // 计算侧边导航标签距离顶部距离
         let top = 100 / (this.categoryList.length) * index;
         this.$refs.sideTagRef.style.top = `${ top }%`;
+        // 切换子分类列表
+        this.categorySubList = item.bxMallSubDto;
+        // 切换子分类下标为第一个
+        this.currentTabIndex = 0;
       }
     }
   }
--- mall/src/store/state.js
+++ mall/src/store/state.js
@@ -2,7 +2,7 @@ import { categoryCache } from '@/assets/js/cache';
 
 const state = {
   goodsDetails: {}, // 商品详情
-  categoryList: categoryCache.getCache(), // 分类
+  categoryList: categoryCache.getCache(), // 大分类列表
 };
 
 export default state;
