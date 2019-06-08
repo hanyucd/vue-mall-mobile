df90184bae02b73fab39ba78c9dfefc6ae135f19
han yu <1807003141@qq.com>
2019-06-08 17:29:16 +0800

    feat: 城市板块结构布局.

--- mall/src/pages/City/index.vue
+++ mall/src/pages/City/index.vue
@@ -8,12 +8,29 @@
     <section class="city-box">
       <b-scroll class="content-scroll">
         <div class="container">
+          <!-- 当前城市 -->
           <section class="location-city">
             <p class="title">当前城市</p>
             <div class="city-list">
               <div class="city-wrapper">
                 <p class="city-name">杭州</p>
               </div>
+            </div>
+          </section>
+          <!-- 热门城市 -->
+          <section class="hot-city">
+            <p class="title">热门城市</p>
+            <div class="city-list">
+              <div class="city-wrapper" v-for="item of cities.data.hotCities" :key="item.id">
+                <p class="city-name">{{ item.name }}</p>
+              </div>
+            </div>
+          </section>
+          <!-- 更多城市 -->
+          <section class="more-city" v-for="(value, key) of cities.data.cities" :key="key">
+            <p class="title">{{ key }}</p>
+            <div class="city-list" v-for="item of value" :key="item.id">
+              <p class="city-name">{{ item.name }}</p>
             </div>
           </section>
         </div>
