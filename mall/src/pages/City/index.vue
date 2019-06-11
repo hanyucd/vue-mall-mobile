714d1ee7959f3970f49aeb7274bf53a3ca8abc75
han yu <1807003141@qq.com>
2019-06-11 17:28:19 +0800

    feat: 完成城市搜索功能.

--- mall/src/pages/City/index.vue
+++ mall/src/pages/City/index.vue
@@ -4,11 +4,13 @@
     
     <section class="city-search">
       <van-icon class="search-icon" name="search" />
-      <input placeholder="请输入城市关键字" v-model="cityKeyword"/>
+      <input placeholder="请输入城市关键字" v-model="cityKeyword" />
+      <van-icon class="clear-icon" name="clear" v-show="cityKeyword" @click="clearSearchInput" />
     </section>
 
     <section class="city-box">
-      <b-scroll class="content-scroll">
+      <!-- 城市列表 -->
+      <b-scroll class="content-scroll" v-show="!cityKeyword">
         <div class="container">
           <!-- 当前城市 -->
           <section class="location-city">
@@ -37,14 +39,26 @@
           </section>
         </div>
       </b-scroll>
+
+      <!-- 搜索相关城市结果 -->
+      <b-scroll class="content-scroll" v-show="cityKeyword" :data="searchCityList">
+        <ul class="search-list">
+          <li class="search-item" v-for="item of searchCityList" :key="item.id" @click="setLocCity(item.name)">
+            {{ item.name }}
+          </li>
+        </ul>
+        <!-- 无相关搜索结果 -->
+        <article class="null-search" v-show="!searchCityList.length">暂时没有相关数据~~</article>
+      </b-scroll>
     </section>
   </div>
 </template>
 
 <script>
+  import cityData from '@/assets/js/city';
   import BScroll from '@/components/BScroll';
   import { GoodsMixin } from '@/mixins/goodsMixin';
-  import cityData from '@/assets/js/city';
+  import { throttle } from '@/utils/tools'; // 导入节流函数
 
   export default {
     name: 'City',
@@ -54,10 +68,14 @@
       return {
         cities: cityData, // 城市数据
         cityKeyword: '', // 搜索城市关键字
+        searchCityList: [], // 搜索城市列表
       };
     },
     watch: {
-      cityKeyword() {}
+      // 监听输入框变化做函数节流 实现 搜索联想
+      cityKeyword() {
+        throttle(this._searchCity(), 300, 500);
+      }
     },
     methods: {
       /**
@@ -66,7 +84,27 @@
       setLocCity(cityName) {
         this.setLocationCity(cityName);
         // this.back();
-      }
+      },
+      /**
+       * 搜索相关城市
+       */
+      _searchCity() {
+        let result = [];
+        let cities = this.cities;
+
+        for (let key in cities.data.cities) {
+          cities.data.cities[key].forEach(item => {
+            // includes()：返回布尔值，表示是否找到了参数字符串。
+            (item.spell.includes(this.cityKeyword) || item.name.includes(this.cityKeyword)) && result.push(item);
+          });
+        }
+
+        this.searchCityList = result;
+      },
+      /**
+       * 清空搜索框关键字
+       */
+      clearSearchInput() { this.cityKeyword = ''; }
     }
   }
 </script>
