100b6fdf1cacf910ef51c5729ab4e9fbf0a938c5
han yu <1807003141@qq.com>
2019-09-19 16:45:16 +0800

    feat: 完成用户界面前端模板.

--- mall/src/pages/User/index.vue
+++ mall/src/pages/User/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
+  <div class="user">
     <top-bar title="会员中心"></top-bar>
-    <div class="dev">{{ msg }}</div>
+    <section class="user-info">
+      <img class="avatar" src="http://img4.imgtn.bdimg.com/it/u=198369807,133263955&fm=27&gp=0.jpg" />
+      <p>登录 / 注册</p>
+    </section>
+    <!-- 订单索引 -->
+    <section class="order-index-wrapper">
+      <div class="order-index-list">
+        <section class="order-all">
+          <span>全部订单</span>
+          <van-icon class="icon" name="arrow" />
+        </section>
+        <ul>
+          <li class="order-index-item" v-for="(item, index) in orderIndex" :key="item.status">
+            <van-icon class="icon" :name="item.icon" />
+            <p class="title">{{ item.title }}</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <!-- 其他：收藏商品、地址管理、最近浏览 -->
+    <section class="other-entry">
+      <div class="colection">
+        <van-icon class="mark-icon" name="star-o" />
+        <span>收藏商品</span>
+        <van-icon class="arrow-icon" name="arrow" />
+      </div>
+      <div class="colection">
+        <van-icon class="mark-icon" name="star-o" />
+        <span>地址管理</span>
+        <van-icon class="arrow-icon" name="arrow" />
+      </div>
+      <div class="colection">
+        <van-icon class="mark-icon" name="eye-o" />
+        <span>最近浏览</span>
+        <van-icon class="arrow-icon" name="arrow" />
+      </div>
+    </section>
     <!-- 底部导航 -->
     <footer-nav></footer-nav>
   </div>
@@ -12,19 +48,23 @@
   import FooterNav from '@/components/FooterNav';
 
   export default {
-    name: 'Me',
+    name: 'User',
     components: { TopBar, FooterNav },
     data() {
       return {
-        msg: '开发中...'
+        // 待付款 1，待发货 2，待收货 3，评价 4，已完成
+        orderIndex: [
+          { icon: "balance-pay", status: 1, title: "待付款" },
+          { icon: "free-postage", status: 2, title: "待发货" },
+          { icon: "points", status: 3, title: "待收货" },
+          { icon: "thumb-circle-o", status: 4, title: "评价" },
+          { icon: "like-o", status: 5, title: "已完成" }
+        ]
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .dev {
-    text-align: center;
-    margin-top: 50px;
-  }
+  @import './style.scss';
 </style>
