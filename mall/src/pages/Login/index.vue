14af3eda6058828a6903d55a0e4b5ae796a5e86d
han yu <1807003141@qq.com>
2019-09-21 20:44:48 +0800

    feat: 定义用户数据模型.

--- mall/src/pages/Login/index.vue
+++ mall/src/pages/Login/index.vue
@@ -5,18 +5,18 @@
       <article class="form-container" v-if="switchFlag === 1">
         <h2 class="title">登录</h2>
         <!-- 用户名 -->
-        <section class="input-wrapper">
-          <input type="text" class="u-name" maxlength="10" placeholder="用户名" autocomplete="off" />
+        <section class="input-wrapper" :class="[ focusIndex === 1 ? 'focus-a' : '' ]">
+          <input type="text" class="u-name" @focus="handleFocus(1)" @blur="handleBlur" maxlength="10" placeholder="用户名" autocomplete="off" />
         </section>
         <!-- 密码 -->
-        <section class="input-wrapper">
-          <input type="password" class="u-password" maxlength="10" placeholder="密码" autocomplete="off" />
+        <section class="input-wrapper" :class="[ focusIndex === 2 ? 'focus-a' : '' ]">
+          <input type="password" class="u-password" @focus="handleFocus(2)" @blur="handleBlur" maxlength="10" placeholder="密码" autocomplete="off" />
         </section>
         <!-- 登录按钮 -->
         <section class="login-btn">登录</section>
         <!-- 前往注册 -->
         <section class="register-entry">
-          <span @click="switchFlag = 2">注册账号</span>
+          <span @click="switchForm(2)">注册账号</span>
         </section>
       </article>
     </transition>
@@ -26,18 +26,18 @@
       <article class="form-container" v-if="switchFlag === 2">
         <h2 class="title">注册</h2>
         <!-- 用户名 -->
-        <section class="input-wrapper">
-          <input type="text" class="u-name" maxlength="10" placeholder="用户名" autocomplete="off" />
+        <section class="input-wrapper" :class="[ focusIndex === 1 ? 'focus-a' : '' ]">
+          <input type="text" class="u-name" @focus="handleFocus(1)" @blur="handleBlur" maxlength="10" placeholder="用户名" autocomplete="off" />
         </section>
         <!-- 密码 -->
-        <section class="input-wrapper">
-          <input type="password" class="u-password" maxlength="10" placeholder="密码" autocomplete="off" />
+        <section class="input-wrapper" :class="[ focusIndex === 2 ? 'focus-a' : '' ]">
+          <input type="password" class="u-password" @focus="handleFocus(2)" @blur="handleBlur" maxlength="10" placeholder="密码" autocomplete="off" />
         </section>
         <!-- 注册按钮 -->
         <section class="login-btn">注册</section>
         <!-- 前往登录 -->
-        <section class="register-entry">
-          <span @click="switchFlag = 1">登录账号</span>
+        <section class="login-entry">
+          <span @click="switchForm(1)">登录账号</span>
         </section>
       </article>
     </transition>
@@ -50,6 +50,24 @@
     data() {
       return {
         switchFlag: 1, // 登录: 1, 注册: 2
+        focusIndex: 0, // 输入框聚焦索引
+      }
+    },
+    methods: {
+      /**
+       * 处理输入框聚焦
+       */
+      handleFocus(index) { this.focusIndex = index; },
+      /**
+       * 处理输入框失焦
+       */
+      handleBlur() { this.focusIndex = 0; },
+      /**
+       * 切换表单
+       */
+      switchForm(flag) {
+        this.switchFlag = flag;
+        this.focusIndex = 0; // 重置表单索引
       }
     }
   }
--- server/models/user.js
+++ server/models/user.js
@@ -1,46 +1,22 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt'); // 用于密码哈希的加密算法
 const Schema = mongoose.Schema;
+
 let ObjectId = Schema.Types.ObjectId; //声明 Object 类型
-const SALT_WORK_FACTOR = 10; // 定义加密密码计算强度
 
 // 创建用户 Schema
 const userSchema = new Schema({
   userId: ObjectId, // 主键
-  userName: { type: String, unique: true }, // unique 表示唯一
-  passWord: String,
-  cart: Array,
-  createAt: { type: Date, default: Date.now() },
-  lastLoginAt: { type: Date, default: Date.now() }
+  userName: { type: String, unique: true }, //  用户名 | unique 表示唯一
+  password: String, // 密码
+  mobile: String, // 手机号码
+  email: String, // 邮箱
+  gender: { type: String, default: '男', enum: [ '男', '女', '保密' ] }, // 性别
+  avatar: { type: String, default: 'http://img4.imgtn.bdimg.com/it/u=198369807,133263955&fm=27&gp=0.jpg' }, // 头像
+  year: { type: Number, default: new Date().getFullYear() },
+  month: { type: Number, default: new Date().getMonth() + 1 },
+  day: { type: Number, default: new Date().getDate() },
+  createAt: { type: Date, default: Date.now() }, // 创建数据的时间
 });
-
-/**
- * 对密码进行加盐
- * 使用 pre 中间件在用户信息存储前执行
- */
-userSchema.pre('save', function(next) {
-  //进行加密
-  // bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-  //   if (err) return next(err);
-  //   bcrypt.hash(this.passWord, salt, (err, hash) => {
-  //     if (err) return next(err);
-  //     this.passWord = hash;
-  //     next();
-  //   });
-  // });
-});
-
-/**
- * 密码比对的方法
- * 第一个参数：客户端传递的; 第二个参数：数据库的
- */
-userSchema.methods.comparePassword = (_passWrod, passWord) => {
-  // return new Promise((resolve, reject) => {
-  //   bcrypt.compare(_passWrod, passWord, (error, result) => {
-  //     !error ? resolve(result) : reject(error);
-  //   });
-  // });
-};
 
 //发布模型
 module.exports = mongoose.model('User', userSchema);
