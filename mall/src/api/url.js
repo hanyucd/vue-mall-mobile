3552b85254d341cf4a784f9c42257bb9bcb79d61
han yu <1807003141@qq.com>
2019-10-08 02:44:15 +0800

    更改服务器部署所需要的配置.

--- mall/src/api/url.js
+++ mall/src/api/url.js
@@ -1,9 +1,9 @@
 // const baseURL = 'http://localhost:3000';
 
-const baseURL = 'http://192.168.1.106:3000'; // 开发使用端口，方便用于真机访问
+// const baseURL = 'http://192.168.1.106:3000'; // 开发使用端口，方便用于真机访问
 // const baseURL = 'http://192.168.43.164:3000'; // 真机测试访问
 
-// const baseURL = 'http://132.232.76.126:8000'; // 服务器临时部署端口
+const baseURL = 'http://132.232.76.126:8000'; // 服务器临时部署端口
 
 export const Url = {
   homeDataApi: baseURL + '/api/goods/home', // 商城首页所有数据
